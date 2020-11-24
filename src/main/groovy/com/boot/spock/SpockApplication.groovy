00bbf685a5f5e57c68df213233beb3b0451d6b67
adhiti2006 <prag2013>
2020-11-24 17:20:11 +0530

    [Added initial set of files]

---
+++ src/main/groovy/com/boot/spock/SpockApplication.groovy
@@ -0,0 +1,13 @@
+package com.boot.spock
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpockApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpockApplication, args)
+	}
+
+}
